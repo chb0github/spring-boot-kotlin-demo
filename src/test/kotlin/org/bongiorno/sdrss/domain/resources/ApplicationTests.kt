20427cb374ce6d1ac17a9d7d16028735cf683f3c
Christian Bongiorno <christian.bongiorno@gmail.com>
2018-02-26 10:31:35 -0800

    Add validation failure test. Currently not working.

--- src/test/kotlin/org/bongiorno/sdrss/domain/resources/ApplicationTests.kt
+++ src/test/kotlin/org/bongiorno/sdrss/domain/resources/ApplicationTests.kt
@@ -1,5 +1,6 @@
 package org.bongiorno.sdrss.domain.resources
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -10,6 +11,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.hateoas.Resource
 import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
@@ -28,5 +30,31 @@ class ApplicationTests(@Autowired private val restTemplate: TestRestTemplate) {
 		assertThat(actual.body!!.content).isEqualTo(expected)
 
 	}
+
+	@Test
+	fun testInvalidPayload() {
+		val input = Employee("Christian", "not and email")
+
+		val response = restTemplate
+				.postForEntity("/employees", input, ValidationFailure::class.java)
+
+		assertThat(response.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+
+		val expected = ValidationFailure(listOf(ValidationError("Employee", "email", input.email))))
+
+		assertThat(response.body).isEqualTo(expected)
+
+
+	}
+
+	class ValidationFailure(@JsonProperty("errors")
+							val errors: List<ValidationError>)
+
+	class ValidationError(
+			var entity: String? = null,
+			var property: String? = null,
+			// don't care what the message is
+			//		public String message;
+			var invalidValue: Any? = null)
 
 }
